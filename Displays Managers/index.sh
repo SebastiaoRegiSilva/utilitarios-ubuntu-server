f36b8a69443fe283498cb893ce194d0b24ca631b
Sebastião Willian <swillian2009@gmail.com>
2024-12-02 20:59:41 +0000

    Correção de diretórios.

---
+++ Displays Managers/index.sh
@@ -0,0 +1,7 @@
+sudo apt update && sudo apt upgrade
+# Instalar o Display Manager.
+# Link com várias informações úteis.
+# https://www.baeldung.com/linux/display-managers-install-uninstall
+sudo apt install slim
+#Instalar o Ubuntu-Desktop.
+sudo apt install ubuntu-desktop
